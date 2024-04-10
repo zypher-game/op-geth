e97d7664573875f10905b5445f21cd2211b06b60
allen-cmyk <allen-cmyk@email.com>
2024-04-10 14:12:15 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR="/data"
+
+# look for empty dir
+if [ "$(ls -A $DIR)" ]; then
+    echo "$DIR is not Empty"
+else
+    echo "$DIR is Empty"
+    ./geth init --datadir="/data" ./genesis.json
+fi
+
+./geth \
+--datadir="/data" \
+--port "30303" \
+--http \
+--http.corsdomain="*" \
+--http.vhosts="*" \
+--http.addr="0.0.0.0" \
+--http.port="8545" \
+--http.api=web3,debug,eth,txpool,net,engine \
+--ws \
+--ws.addr "0.0.0.0" \
+--ws.port "8546" \
+--ws.api=web3,debug,eth,txpool,net,engine \
+--ws.origins "*" \
+--syncmode=full \
+--gcmode=archive \
+--nodiscover \
+--maxpeers=0 \
+--authrpc.vhosts="*" \
+--authrpc.addr="0.0.0.0" \
+--authrpc.port="8551" \
+--authrpc.jwtsecret="/opt/secret.txt" \
+--rollup.disabletxpoolgossip=true
